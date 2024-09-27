f0c09cab0c29b06a2bec9e4d408ced4bdc4bce00
Jonnathan Morales Yate <jonnathanmoralesyate@hotmail.com>
2024-09-26 19:01:25 -0500

    avance el login y enlace de ubicacion

---
+++ js/LoginInicio.js
@@ -0,0 +1,42 @@
+
+//indica que el código dentro de esta función solo se ejecutará cuando todo el documento HTML haya sido cargado por completo.
+//Es útil para asegurarse de que los elementos del DOM (como botones y formularios) estén disponibles antes de intentar acceder a ellos.
+
+document.addEventListener('DOMContentLoaded', function() {
+
+//Función pare el botón Login: Muestra el formulario de inicio: 
+
+    document.getElementById("login_inic").addEventListener('click', function() {
+        
+        var elemento = document.getElementById("login_form");
+
+        if (elemento.style.display === "none" || elemento.style.display === "") {
+            elemento.style.display = "block"; // Muestra el elemento
+
+        } else {
+            elemento.style.display = "none"; // Oculta el elemento si ya está visible
+        }
+        
+    });
+});
+
+//Función para el botón cerrarL (X) : Cierra el formulario de inicio de sesión.
+
+document.addEventListener('DOMContentLoaded',function(){
+    document.getElementById("cerrarL").addEventListener('click', function(){
+
+        //alert("pulso x");
+
+        let elemento = document.getElementById("login_form");
+
+       // elemento.style.display = "none";
+
+        elemento.remove();  // elimina el elemento si ya está visible
+
+        location.reload();  //método de recarga la URL actual, como el botón Actualizar.
+
+    });
+
+    
+
+});
